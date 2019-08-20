5f9f0d8522eba9910e78ee33475d2f7f0889e625
Tim Liu <timliu92@gmail.com>
2019-08-19 22:44:01 -0400

    expanded TestContainers.java to include arrays and hashmaps

--- Lecture2/Playground/TestContainers.java
+++ Lecture2/Playground/TestContainers.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 // class for testing containers
 
@@ -7,6 +7,7 @@ public class TestContainers {
 	public static void main(String[] args) {
 		System.out.println("testing different containers");
 		testList();
+		// testMap();
 	}
 	/* test ArrayLists */
 	public static void testList() {
@@ -14,9 +15,21 @@ public class TestContainers {
 		ArrayList<Integer> newList = new ArrayList<Integer>();
 		newList.add(6);
 		newList.add(9);
+		newList.add(12);
 
 		for (int i = 0; i <newList.size(); i++) {
 			System.out.println(newList.get(i));
+		}
+
+		// test iterator
+		Iterator<Integer> iter = newList.iterator();
+		do {
+			System.out.println(iter.next());
+		} while(iter.hasNext());
+
+		// test enhanced for - loop syntax
+		for (Integer i: newList) {
+			System.out.println("New element!: " + i);
 		}
 		return;
 	}
@@ -32,6 +45,35 @@ public class TestContainers {
 			System.out.println(myArray[i]);
 		}
 		return;
+	}
+	// test maps
+	public static void testMap() {
+		System.out.println("Testing HashMaps");
+		HashMap<String, String> stateCapitals = new HashMap<>();
 
+		// add some elements to our new map
+		stateCapitals.put("Indiana", "Indianapolis");
+		stateCapitals.put("Delaware", "Dover");
+		stateCapitals.put("Washington", "Olympia");
+
+		// print out the map
+		System.out.println(stateCapitals);
+
+		// get size and print it
+
+		int mapSize = stateCapitals.size(); // line not necessary
+		System.out.println(mapSize);
+		// check if element is in there
+		if (stateCapitals.containsKey("Washington")) {
+			System.out.println(stateCapitals.get("Washington"));
+		}
+
+		return;
 	}
 }
+
+
+
+
+
+
