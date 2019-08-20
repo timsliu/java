75dacfdb9cf500d1aac6c9f819ac359007b08359
Tim Liu <timliu92@gmail.com>
2019-08-19 20:31:36 -0400

    tested lists and arrays in Testcontainers.java

--- Lecture2/Playground/TestContainers.java
+++ Lecture2/Playground/TestContainers.java
@@ -3,12 +3,35 @@ import java.util.ArrayList;
 // class for testing containers
 
 public class TestContainers {
+	/* main function - calls other container test functions */
 	public static void main(String[] args) {
 		System.out.println("testing different containers");
 		testList();
 	}
+	/* test ArrayLists */
 	public static void testList() {
 		System.out.println("List");
 		ArrayList<Integer> newList = new ArrayList<Integer>();
+		newList.add(6);
+		newList.add(9);
+
+		for (int i = 0; i <newList.size(); i++) {
+			System.out.println(newList.get(i));
+		}
+		return;
+	}
+	/* test arrays */
+	public static void testArray() {
+		System.out.println("Testing array");
+		// declare an array of integers
+		int[] myArray;
+		// allocate memory
+		myArray = new int[4];
+		for (int i = 0; i < 4; i ++) {
+			//myArray[i] = i;
+			System.out.println(myArray[i]);
+		}
+		return;
+
 	}
 }
